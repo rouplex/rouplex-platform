8edfaaa63f528bae4fb61a73b2187552470e6261
Andi Mullaraj <andimullaraj@gmail.com>
2017-03-26 12:44:40 -0700

    Bulking up aop reporting

--- rouplex-platform-tcp-instrumentor/src/main/java/org/rouplex/platform/tcp/RouplexTcpBinderReporter.java
+++ rouplex-platform-tcp-instrumentor/src/main/java/org/rouplex/platform/tcp/RouplexTcpBinderReporter.java
@@ -1,9 +1,11 @@
 package org.rouplex.platform.tcp;
 
+import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
 import org.aspectj.lang.ProceedingJoinPoint;
 
+import java.util.ConcurrentModificationException;
 import java.util.logging.Logger;
 
 /**
@@ -26,16 +28,6 @@ class RouplexTcpBinderReporter {
         this.aopInstrumentor = aopInstrumentor;
 
         updateId();
-
-        handleSelectedKey = aopInstrumentor.metricRegistry.meter(MetricRegistry.name(aggregatedId, "handleSelectedKey"));
-
-//        aopInstrumentor.metricRegistry.register(MetricRegistry.name(aggregatedId, "registeredSelectionKeys"),
-//                new Gauge<Integer>() {
-//                    @Override
-//                    public Integer getValue() {
-//                        return rouplexTcpBinder.selector.keys().size();
-//                    }
-//                });
     }
 
     public Object handleSelectedKey(ProceedingJoinPoint pjp) throws Throwable {
@@ -52,6 +44,36 @@ class RouplexTcpBinderReporter {
         aggregatedId = String.format(format,
                 aopConfig.aggregateTcpBinders ? "A" : rouplexTcpBinder.hashCode()
         );
+
+        handleSelectedKey = aopInstrumentor.metricRegistry.meter(MetricRegistry.name(aggregatedId, "handleSelectedKey"));
+
+        try {
+            /**
+             * This section is not perfect since it will always lump together all the TcpBinders. If fetching this
+             * metric becomes important then chaining the gauges would be a solution (or maybe better understanding
+             * yammer).
+             */
+            aopInstrumentor.metricRegistry.register(MetricRegistry.name(aggregatedId, "registeredSelectionKeys"),
+                    new Gauge<Integer>() {
+                        @Override
+                        public Integer getValue() {
+                            int totalKeys = 0;
+
+                            try {
+                                for (RouplexTcpBinder tcpBinder : aopInstrumentor.tcpBinders.keySet()) {
+                                    totalKeys += tcpBinder.selector.keys().size();
+                                }
+                            } catch (ConcurrentModificationException cme) {
+                                // no biggie return whatever, rather than synchronize and get on tests way
+                            }
+
+                            return totalKeys;
+                        }
+                    });
+        } catch (RuntimeException re) {
+            // read the comment at beginning of the block
+        }
+
     }
 
     public String getAggregatedId() {
