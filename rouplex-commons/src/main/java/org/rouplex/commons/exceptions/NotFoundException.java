ce92843ee31476f2411650557c8d0c4758d2e700
Andi Mullaraj <andimullaraj@gmail.com>
2017-07-27 19:14:45 -0700

    Added NotFoundException -- the jax-rs one would come with some baggage

---
+++ rouplex-commons/src/main/java/org/rouplex/commons/exceptions/NotFoundException.java
@@ -0,0 +1,13 @@
+package org.rouplex.commons.exceptions;
+
+/**
+ * @author Andi Mullaraj (andimullaraj at gmail.com)
+ */
+public class NotFoundException extends Exception {
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
